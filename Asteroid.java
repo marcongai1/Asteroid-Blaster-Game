6a6406850b8bbf1be6d5e473a79cc904788284f3
Marco <128193445+marcongai1@users.noreply.github.com>
2023-06-02 10:33:38 -0400

    Update Asteroid.java

--- Asteroid.java
+++ Asteroid.java
@@ -1,5 +1,6 @@
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.KeyAdapter;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
 import java.util.ArrayList;
@@ -13,17 +14,18 @@ public class Game{
     ArrayList projectiles,enemyRectangles,asteroids;
     JFrame frame;
     Timer timer;
+    boolean gameOver;
     public Game(JFrame frame){
         this.frame = frame;
         asteroids = new ArrayList<>();
         projectiles = new ArrayList<>();
         enemyRectangles = new ArrayList<>();
-        shipX = 0;// finish statement
-        shipY = 0;// finish statement
-        playerRectangle = new Rectangle(shipX, shipY, 10, 10);// finish Statement
+        shipX = 250;// finish statement
+        shipY = 500;// finish statement
+        playerRectangle = new Rectangle(shipX, shipY, 50, 50);// finish Statement
 
         frame.setFocusable(true);    
-        frame.addKeyListener(new KeyListener(){
+        frame.addKeyListener(new KeyAdapter(){
             @Override
             public void keyTyped(KeyEvent e) {
                 // TODO Auto-generated method stub
@@ -46,19 +48,58 @@ public class Game{
         timer = new Timer(10, new ActionListener(){
             @Override
             public void actionPerformed(ActionEvent event){
-                //to be implemented
+                if(gameOver){
+                    //implement
+                }
+                else{
+                    updateScreen();
+                    frame.repaint();
+                }
             }
         });
         timer.start();
     }
     public void updateEnemyRectanges(){
-
+        for (int i = 0; i < asteroids.size(); i++) {
+            Asteroid asteroid = (Asteroid) asteroids.get(i);
+            Rectangle enemyRectangle = (Rectangle) enemyRectangles.get(i);
+            int newX = asteroid.getAsteroidX(); 
+            int newY = asteroid.getAsteroidY();  
+            enemyRectangle.setLocation(newX, newY);
+        }
     }
     private void handleKeyPress(KeyEvent event){
-
+        //up
+        if(event.getKeyCode()==38){  
+            if(shipY<50){}
+            else
+                shipY--;
+        }
+        //right
+        if(event.getKeyCode()==39){ 
+            if(shipX>350){}
+            else
+                shipX++;  
+        }
+        //down
+        if(event.getKeyCode()==40){   
+            if(shipY>550){}
+            else
+                shipY++;
+        }
+        //left
+        if(event.getKeyCode()==37){  
+            if(shipX<50){}
+            else   
+                shipX--; 
+        }
+        //space bar shooting
+        if(event.getKeyCode()==32){
+            shoot();
+        }
     }
     private void shoot(){
-
+        
     }
     private void checkForAsteroidCollisions(){
 

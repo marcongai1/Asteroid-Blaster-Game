07292d196c9de92aea770f14df742fa889d3a188
Marco <128193445+marcongai1@users.noreply.github.com>
2023-06-07 19:51:16 -0400

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,6 +1,7 @@
+import javax.swing.JFrame;
 public class Main {
     public static void main(String[]args){
-        JFrame frame = new JFrame(“Asteroid Shooter”);
+        JFrame frame = new JFrame("Asteroid Shooter");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setSize(400, 600);
         frame.setLocationRelativeTo(null);

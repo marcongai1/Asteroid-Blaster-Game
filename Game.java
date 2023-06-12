766c299a29401f6c9898c566474856e809fca38e
Marco <128193445+marcongai1@users.noreply.github.com>
2023-06-12 10:08:21 -0400

    Update Game.java

--- Game.java
+++ Game.java
@@ -11,8 +11,9 @@ import java.awt.Font;
 import java.awt.FontMetrics;
 import java.awt.Graphics;
 import java.awt.Rectangle;
-public class Game extends JComponent{
-    int shipX,shipY,lives,asteroidsHit;
+
+public class Game extends JComponent {
+    int shipX, shipY, lives, asteroidsHit;
     Rectangle playerRectangle;
     ArrayList<Rectangle> enemyRectangles;
     ArrayList<Asteroid> asteroids;
@@ -20,7 +21,8 @@ public class Game extends JComponent{
     JFrame frame;
     Timer timer;
     boolean gameOver;
-    public Game(JFrame frame){
+
+    public Game(JFrame frame) {
         this.frame = frame;
         asteroids = new ArrayList<>();
         projectiles = new ArrayList<>();
@@ -30,47 +32,47 @@ public class Game extends JComponent{
         lives = 3;
         playerRectangle = new Rectangle(shipX, shipY, 50, 50);
 
-        frame.setFocusable(true);    
-        frame.addKeyListener(new KeyAdapter(){
+        frame.setFocusable(true);
+        frame.addKeyListener(new KeyAdapter() {
             @Override
             public void keyTyped(KeyEvent e) {}
 
             @Override
             public void keyPressed(KeyEvent e) {
-                 //up
-                if(e.getKeyCode()==38){  
-                    if(shipY<50){}
-                    else{
+                // up
+                if (e.getKeyCode() == 38) {
+                    if (shipY < 50) {}
+                    else {
                         shipY--;
                         playerRectangle.setLocation(shipX, shipY);
                     }
                 }
-                //right
-                if(e.getKeyCode()==39){ 
-                    if(shipX>350){}
-                    else{
-                        shipX++; 
+                // right
+                if (e.getKeyCode() == 39) {
+                    if (shipX > 350) {}
+                    else {
+                        shipX++;
                         playerRectangle.setLocation(shipX, shipY);
-                    } 
+                    }
                 }
-                //down
-                if(e.getKeyCode()==40){   
-                    if(shipY>550){}
-                    else{
+                // down
+                if (e.getKeyCode() == 40) {
+                    if (shipY > 550) {}
+                    else {
                         shipY++;
                         playerRectangle.setLocation(shipX, shipY);
                     }
                 }
-                //left
-                if(e.getKeyCode()==37){  
-                    if(shipX<50){}
-                    else{
-                        shipX--; 
+                // left
+                if (e.getKeyCode() == 37) {
+                    if (shipX < 50) {}
+                    else {
+                        shipX--;
                         playerRectangle.setLocation(shipX, shipY);
                     }
                 }
-                //space bar shooting
-                if(e.getKeyCode()==32){
+                // space bar shooting
+                if (e.getKeyCode() == 32) {
                     shoot();
                 }
             }
@@ -79,13 +81,12 @@ public class Game extends JComponent{
             public void keyReleased(KeyEvent e) {}
         });
 
-        timer = new Timer(10, new ActionListener(){
+        timer = new Timer(10, new ActionListener() {
             @Override
-            public void actionPerformed(ActionEvent event){
-                if(gameOver){
-                    //implement
-                }
-                else{
+            public void actionPerformed(ActionEvent event) {
+                if (gameOver) {
+                    // implement
+                } else {
                     updateScreen();
                     frame.repaint();
                 }
@@ -93,63 +94,71 @@ public class Game extends JComponent{
         });
         timer.start();
     }
-    public void updateEnemyRectangles(){
+
+    public void updateEnemyRectangles() {
         for (int i = 0; i < asteroids.size(); i++) {
             Asteroid asteroid = asteroids.get(i);
             Rectangle enemyRectangle = enemyRectangles.get(i);
-            int newX = asteroid.getAsteroidX(); 
-            int newY = asteroid.getAsteroidY();  
+            int newX = asteroid.getAsteroidX();
+            int newY = asteroid.getAsteroidY();
             enemyRectangle.setLocation(newX, newY);
         }
     }
-    private void shoot(){
+
+    private void shoot() {
         int shipMidpointX = shipX + 25;
-        int shipMidpointY = shipY + 25;    
+        int shipMidpointY = shipY + 25;
         Projectile projectile = new Projectile(shipMidpointX, shipMidpointY);
-        projectiles.add(projectile);    
+        projectiles.add(projectile);
     }
-    private void checkForAsteroidCollisions(){
-        for(int i = enemyRectangles.size()-1; i >= 0; i--)
-        if(enemyRectangles.get(i).intersects(playerRectangle)){
-            enemyRectangles.remove(i);
-            asteroids.remove(i);
-            lives--;
+
+    private void checkForAsteroidCollisions() {
+        for (int i = enemyRectangles.size() - 1; i >= 0; i--) {
+            if (enemyRectangles.get(i).intersects(playerRectangle)) {
+                enemyRectangles.remove(i);
+                asteroids.remove(i);
+                lives--;
+            }
         }
     }
-    private void generateNewAsteroid(){
-        if(Math.random()*5 == 3){
+
+    private void generateNewAsteroid() {
+        if (Math.random() * 5 == 3) {
             Asteroid asteroid = new Asteroid(this);
             asteroids.add(asteroid);
-            Rectangle enemyRectangles = new Rectangle(asteroid.getAsteroidX(),asteroid.getAsteroidY(),50,50);
-            enemyRectangles.add(enemyRectangles);
+            Rectangle enemyRectangle = new Rectangle(asteroid.getAsteroidX(), asteroid.getAsteroidY(), 50, 50);
+            enemyRectangles.add(enemyRectangle);
         }
     }
-    private void removeAsteroid(int index){
+
+    private void removeAsteroid(int index) {
         asteroids.get(index).setDestroyed(true);
         asteroids.remove(index);
         enemyRectangles.remove(index);
     }
-    private void updateAsteroidLocation(){
-        for(int i = asteroids.size()-1; i >= 0; i--){
+
+    private void updateAsteroidLocation() {
+        for (int i = asteroids.size() - 1; i >= 0; i--) {
             asteroids.get(i).updateAsteroid();
         }
     }
-    private void checkProjectileCollisions(){
+
+    private void checkProjectileCollisions() {
         for (int i = 0; i < asteroids.size(); i++) {
-            Rectangle enermyRectangle = enemyRectangles.get(i);
-    
+            Rectangle enemyRectangle = enemyRectangles.get(i);
+
             for (int j = 0; j < projectiles.size(); j++) {
                 Projectile projectile = projectiles.get(j);
                 int projectileX = projectile.getX();
                 int projectileY = projectile.getY();
-    
+
                 // Check if projectile moves off the screen
                 if (projectileY < 0) {
                     projectiles.remove(j);
                     j--;
                 }
                 // Check if projectile intersects with asteroid
-                else if (enermyRectangle.intersects(projectileX,projectileY,10,10)) {
+                else if (enemyRectangle.intersects(projectileX, projectileY, 10, 10)) {
                     projectiles.remove(j);
                     removeAsteroid(i);
                     asteroidsHit++;
@@ -161,29 +170,30 @@ public class Game extends JComponent{
             }
         }
     }
-    private void updateProjectiles(){
+
+    private void updateProjectiles() {
         ArrayList<Projectile> newProjectiles = new ArrayList<>();
-        for(Projectile projectile : projectiles){
+        for (Projectile projectile : projectiles) {
             projectile.updateProjectilePosition();
             newProjectiles.add(projectile);
         }
         projectiles = newProjectiles;
     }
-    private void updateScreen(){
+
+    private void updateScreen() {
         checkForAsteroidCollisions();
         updateAsteroidLocation();
         generateNewAsteroid();
         updateProjectiles();
         checkProjectileCollisions();
     }
+
     private void drawShip(Graphics graphics) {
-        int shipX = 100;  // X-coordinate of the ship's position
-        int shipY = 100;  // Y-coordinate of the ship's position
         int shipWidth = 50;  // Width of the ship
         int shipHeight = 50;  // Height of the ship
-    
-        graphics.setColor(Color.BLUE);  
-        graphics.fillRect(shipX, shipY, shipWidth, shipHeight); 
+
+        graphics.setColor(Color.BLUE);
+        graphics.fillRect(shipX, shipY, shipWidth, shipHeight);
     }
 
     private void drawAsteroids(Graphics graphics) {
@@ -196,6 +206,7 @@ public class Game extends JComponent{
         }
         updateEnemyRectangles();
     }
+
     private void drawProjectiles(Graphics graphics) {
         for (int i = 0; i < projectiles.size(); i++) {
             Projectile projectile = projectiles.get(i);
@@ -203,7 +214,7 @@ public class Game extends JComponent{
             graphics.fillRect(projectile.getX(), projectile.getY(), 10, 10);
         }
     }
-    
+
     private void setEndScreenText(Graphics graphics, String str) {
         graphics.setColor(Color.WHITE);
         graphics.setFont(new Font("Arial", Font.BOLD, 24));
@@ -213,21 +224,20 @@ public class Game extends JComponent{
         int y = getHeight() / 2;
         graphics.drawString(str, x, y);
     }
-    
+
     private void setGameOver(Graphics graphics) {
         setEndScreenText(graphics, "Game Over");
     }
-    
+
+    @Override
     protected void paintComponent(Graphics graphics) {
         super.paintComponent(graphics);
         drawAsteroids(graphics);
         drawProjectiles(graphics);
         drawShip(graphics);
-    
+
         if (gameOver) {
             setGameOver(graphics);
         }
     }
-    
-
 }
